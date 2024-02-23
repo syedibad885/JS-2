61aa42bd97578878cd572ea9e000912f12cbba0c
Syed ibad <155752000+syedibad885@users.noreply.github.com>
2024-02-23 19:41:24 +0500

    Add files via upload

---
+++ ibad.js
@@ -0,0 +1,54 @@
+let otherName = "// undefind"
+
+let myNamde = "syedibad"
+
+
+
+console.log("my name is ", myNamde);
+
+
+// change varriable value
+myName = "ibad";
+
+console.log(myName);
+
+myName = "testing";
+
+
+
+
+
+
+
+let userName = prompt("what is your name");
+alert("welcome" + userName)
+
+// promot se hum user se input lete hyn
+let num1 = prompt("enter first number")
+let num2 = prompt("enter second number")
+
+
+//string ko number ma convert kia HY
+num1 = Number(num1);
+num2 = Number(num2);
+
+
+console.log("num1", num1)
+console.log("num2", num2)
+
+let sum =num1 + num2;
+let minus =num1 - num2;
+
+alert("addition" + sum);
+alert("subtraction" + minus);
+alert("multipication" + num1 * num2);
+alert("division"+ num1 / num2);
+
+
+
+
+
+
+
+
+
